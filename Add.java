77884f409141ec16f91af936f2934770065f2a6a
Komal Wani <78967634+komalw20@users.noreply.github.com>
2021-05-03 10:43:54 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ public class Add
+{
+    public static void main(String args[])
+    {
+        int x=5;
+        int y=6,z;
+        z=x+y;
+        System.out.println("The addition is="+z);
+    }
+}
+
+
+/*output
+The addition is=11*/
